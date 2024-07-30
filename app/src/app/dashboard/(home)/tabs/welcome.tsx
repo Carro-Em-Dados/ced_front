bb14ae22aebb756da995481034a628ffb26936cd
MarcosGaius <gaiusmarcos@gmail.com>
2024-07-30 11:20:59 -0300

    fix: adding non-null assert

--- app/src/app/dashboard/(home)/tabs/welcome.tsx
+++ app/src/app/dashboard/(home)/tabs/welcome.tsx
@@ -10,104 +10,87 @@ import { AuthContext } from "@/contexts/auth.context";
 import { toast, Zoom } from "react-toastify";
 
 export default function Welcome() {
-	const [workshops, setWorkshops] = useState<Workshop[]>([]);
-	const [selectedWorkshop, setSelectedWorkshop] = useState<string | null>(null);
-	const { currentUser, currentWorkshop, db } = useContext(AuthContext);
+  const [workshops, setWorkshops] = useState<Workshop[]>([]);
+  const [selectedWorkshop, setSelectedWorkshop] = useState<string | null>(null);
+  const { currentUser, currentWorkshop, db } = useContext(AuthContext);
 
-	useEffect(() => {
-		getWorkshops();
-	}, [currentUser, currentWorkshop]);
+  useEffect(() => {
+    getWorkshops();
+  }, [currentUser, currentWorkshop]);
 
-	const getWorkshops = async () => {
-		if (!currentUser || (!currentWorkshop && currentUser.role !== "master"))
-			return;
-		try {
-			if (currentUser.role === "master") {
-				const querySnapshot = await getDocs(collection(db, "workshops"));
-				if (!querySnapshot.empty) {
-					const data = querySnapshot.docs.map((doc) => ({
-						...(doc.data() as Workshop),
-						id: doc.id,
-					}));
-					setWorkshops(data);
-				}
-			} else {
-				const docRef = doc(db, "workshops", currentWorkshop.id);
-				const docSnap = await getDoc(docRef);
-				if (docSnap.exists()) {
-					const data = { ...(docSnap.data() as Workshop), id: docSnap.id };
-					setWorkshops([data]);
-				}
-			}
-		} catch (error) {
-			toast.error("Algo deu errado!", {
-				position: "bottom-right",
-				autoClose: 5000,
-				hideProgressBar: true,
-				closeOnClick: true,
-				pauseOnHover: true,
-				draggable: true,
-				progress: undefined,
-				theme: "dark",
-				transition: Zoom,
-			});
-		}
-	};
+  const getWorkshops = async () => {
+    if (!currentUser || (!currentWorkshop && currentUser.role !== "master")) return;
+    try {
+      if (currentUser.role === "master") {
+        const querySnapshot = await getDocs(collection(db, "workshops"));
+        if (!querySnapshot.empty) {
+          const data = querySnapshot.docs.map((doc) => ({
+            ...(doc.data() as Workshop),
+            id: doc.id,
+          }));
+          setWorkshops(data);
+        }
+      } else {
+        const docRef = doc(db, "workshops", currentWorkshop!.id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = { ...(docSnap.data() as Workshop), id: docSnap.id };
+          setWorkshops([data]);
+        }
+      }
+    } catch (error) {
+      toast.error("Algo deu errado!", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Zoom,
+      });
+    }
+  };
 
-	return (
-		<div className={styles.pageWrap}>
-			<div className={styles.textContainer}>
-				<div className={styles.titleContainer}>
-					<div className={styles.rectangleContainer}>
-						<Image
-							src="/rectangle.png"
-							alt="Retângulo título"
-							fill
-							style={{ objectFit: "cover" }}
-						/>
-					</div>
-					{currentUser?.name ? (
-						<h1 className={styles.mainTitle}>{`Olá, ${currentUser?.name}!`}</h1>
-					) : (
-						<h1 className={styles.mainTitle}>Olá!</h1>
-					)}
-				</div>
-				<p className={styles.subtext}>
-					Gostaria de conferir o status da sua oficina?
-				</p>
-				<p className={styles.subtext}>Selecione alguma opção abaixo:</p>
-				<div className={styles.dropdownContainer}>
-					<DropdownComponent
-						options={workshops?.map((workshop) => ({
-							label: workshop.fantasy_name,
-							key: workshop.id,
-						}))}
-						placeholder="Selecione sua oficina"
-						value={selectedWorkshop}
-						onChange={(key) => {
-							setSelectedWorkshop(key.toString());
-						}}
-					/>
-				</div>
-			</div>
-			{selectedWorkshop ? (
-				<Dashboard selectedWorkshop={selectedWorkshop} />
-			) : (
-				<WelcomeImage />
-			)}
-		</div>
-	);
+  return (
+    <div className={styles.pageWrap}>
+      <div className={styles.textContainer}>
+        <div className={styles.titleContainer}>
+          <div className={styles.rectangleContainer}>
+            <Image src="/rectangle.png" alt="Retângulo título" fill style={{ objectFit: "cover" }} />
+          </div>
+          {currentUser?.name ? (
+            <h1 className={styles.mainTitle}>{`Olá, ${currentUser?.name}!`}</h1>
+          ) : (
+            <h1 className={styles.mainTitle}>Olá!</h1>
+          )}
+        </div>
+        <p className={styles.subtext}>Gostaria de conferir o status da sua oficina?</p>
+        <p className={styles.subtext}>Selecione alguma opção abaixo:</p>
+        <div className={styles.dropdownContainer}>
+          <DropdownComponent
+            options={workshops?.map((workshop) => ({
+              label: workshop.fantasy_name,
+              key: workshop.id,
+            }))}
+            placeholder="Selecione sua oficina"
+            value={selectedWorkshop}
+            onChange={(key) => {
+              setSelectedWorkshop(key.toString());
+            }}
+          />
+        </div>
+      </div>
+      {selectedWorkshop ? <Dashboard selectedWorkshop={selectedWorkshop} /> : <WelcomeImage />}
+    </div>
+  );
 }
 
 const WelcomeImage = () => {
-	return (
-		<div className={styles.imageContainer}>
-			<Image
-				src="/car_home1.png"
-				alt="Carro na Home Principal"
-				fill
-				style={{ objectFit: "cover", objectPosition: "right 20% bottom 0" }}
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.imageContainer}>
+      <Image src="/car_home1.png" alt="Carro na Home Principal" fill style={{ objectFit: "cover", objectPosition: "right 20% bottom 0" }} />
+    </div>
+  );
 };
