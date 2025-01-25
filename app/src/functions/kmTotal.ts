96e7f36f173616d98cf7641248bd00f37c988aed
yellowisk <oheitoralmeida2013@gmail.com>
2025-01-25 17:35:44 -0300

    fix(kmTotal): prevent NaN result

--- app/src/functions/kmTotal.ts
+++ app/src/functions/kmTotal.ts
@@ -2,7 +2,7 @@ import { Vehicle } from "@/interfaces/vehicle.type";
 import { Reading } from "@/interfaces/readings.type";
 
 export function getTotalKm(vehicle: Vehicle, reading: Reading): number {
-  const readingDistance = reading.obd2_distance || reading.gps_distance;
-  const km_current = vehicle.initial_km + readingDistance;
+  const readingDistance = reading?.obd2_distance || reading?.gps_distance;
+  const km_current = vehicle.initial_km + (readingDistance ?? 0);
   return km_current;
 }
