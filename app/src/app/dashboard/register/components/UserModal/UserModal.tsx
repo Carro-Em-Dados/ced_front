7b150adfa8a93b114dc591e91ad230367c8c25ed
yellowisk <oheitoralmeida2013@gmail.com>
2025-02-12 18:56:54 -0300

    feat(UserModal): assign user to workshop if it has a basic role

--- app/src/app/dashboard/register/components/UserModal/UserModal.tsx
+++ app/src/app/dashboard/register/components/UserModal/UserModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   Button,
   Modal,
@@ -14,11 +14,24 @@ import {
 import { MdLibraryAdd } from "react-icons/md";
 import styles from "../../styles.module.scss";
 import clsx from "clsx";
-import { doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
 import { AuthContext } from "@/contexts/auth.context";
-import { createUserWithEmailAndPassword, getAuth, deleteUser } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  deleteUser,
+} from "firebase/auth";
 import { toast, Zoom } from "react-toastify";
 import { roleLabel } from "@/constants/rolesLabel";
+import { Role } from "@/types/enums/role.enum";
+import { Workshop } from "@/interfaces/workshop.type";
 
 interface Props {
   setUsers: React.Dispatch<React.SetStateAction<any[]>>;
@@ -32,23 +45,63 @@ export default function UserModal({ setUsers }: Props) {
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("user");
   const [loading, setLoading] = useState(false);
-  const filteredRoleLabels = currentUser?.role === "master" 
-    ? roleLabel 
-    : Object.fromEntries(Object.entries(roleLabel).filter(([key]) => key === "user"));
+  const filteredRoleLabels =
+    currentUser?.role === "master"
+      ? roleLabel
+      : Object.fromEntries(
+          Object.entries(roleLabel).filter(([key]) => key === "user")
+        );
+  const [workshop, setWorkshop] = useState<Workshop[]>([]);
+  const [workshopId, setWorkshopId] = useState("");
 
+  useEffect(() => {
+    const fetchWorkshops = async () => {
+      if (currentUser?.role === Role.MASTER) {
+        const workshopsRef = collection(db, "workshops");
+        const workshopsSnapshot = await getDocs(workshopsRef);
+        const workshopsList = workshopsSnapshot.docs.map(
+          (doc) => doc.data() as Workshop
+        );
+        setWorkshop(workshopsList);
+      }
+
+      if (currentUser?.role === Role.ORGANIZATION) {
+        const workshopsRef = query(
+          collection(db, "workshops"),
+          where("owner", "==", currentUser.id)
+        );
+        const workshopsSnapshot = await getDocs(workshopsRef);
+        const workshopsList = workshopsSnapshot.docs.map(
+          (doc) => doc.data() as Workshop
+        );
+        setWorkshop(workshopsList);
+      }
+    };
+    fetchWorkshops();
+  }, [currentUser, db]);
+
+  useEffect(() => {
+    if (role !== "user") {
+      setWorkshopId("");
+    }
+  }, [role]);
 
   const addUser = async () => {
     setLoading(true);
     try {
       const auth = getAuth();
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
       const newUser = {
         email: userCredential.user.email!,
         name: name,
         id: userCredential.user.uid,
         role: role,
-        workshops: "",
+        workshops: workshopId || "",
       };
 
       const docRef = doc(db, "users", newUser.id);
@@ -83,15 +136,32 @@ export default function UserModal({ setUsers }: Props) {
 
   return (
     <>
-      <Button color="success" className={clsx(styles.button, "!fixed bottom-0 right-0 z-10 shadow-md mb-5 mr-5")} onClick={onOpen}>
+      <Button
+        color="success"
+        className={clsx(
+          styles.button,
+          "!fixed bottom-0 right-0 z-10 shadow-md mb-5 mr-5"
+        )}
+        onClick={onOpen}
+      >
         <MdLibraryAdd className={styles.addIcon} />
         Adicionar usuário
       </Button>
-      <Modal isOpen={isOpen} className={styles.modal} onClose={handleClose} size={"lg"} onOpenChange={onOpenChange}>
+      <Modal
+        isOpen={isOpen}
+        className={styles.modal}
+        onClose={handleClose}
+        size={"lg"}
+        onOpenChange={onOpenChange}
+      >
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className={clsx("flex flex-col gap-1", styles.modalTitle)}>Adicionar Usuário</ModalHeader>
+              <ModalHeader
+                className={clsx("flex flex-col gap-1", styles.modalTitle)}
+              >
+                Adicionar Usuário
+              </ModalHeader>
               <ModalBody>
                 <div className={clsx(styles.form, "flex flex-col gap-4")}>
                   <div>
@@ -104,7 +174,9 @@ export default function UserModal({ setUsers }: Props) {
                       className="dark"
                       classNames={{
                         input: ["bg-transparent text-white"],
-                        inputWrapper: ["border border-2 !border-white focus:border-white"],
+                        inputWrapper: [
+                          "border border-2 !border-white focus:border-white",
+                        ],
                       }}
                     />
                   </div>
@@ -118,7 +190,9 @@ export default function UserModal({ setUsers }: Props) {
                       className="dark"
                       classNames={{
                         input: ["bg-transparent text-white"],
-                        inputWrapper: ["border border-2 !border-white focus:border-white"],
+                        inputWrapper: [
+                          "border border-2 !border-white focus:border-white",
+                        ],
                       }}
                     />
                   </div>
@@ -132,7 +206,9 @@ export default function UserModal({ setUsers }: Props) {
                       className="dark"
                       classNames={{
                         input: ["bg-transparent text-white"],
-                        inputWrapper: ["border border-2 !border-white focus:border-white"],
+                        inputWrapper: [
+                          "border border-2 !border-white focus:border-white",
+                        ],
                       }}
                     />
                   </div>
@@ -147,20 +223,53 @@ export default function UserModal({ setUsers }: Props) {
                       value={role}
                       onChange={(e) => setRole(e.target.value)}
                     >
-                      {Object.entries(filteredRoleLabels).map(([key, value]) => (
-                        <SelectItem key={key} value={key}>
-                          {value}
-                        </SelectItem>
-                      ))}
+                      {Object.entries(filteredRoleLabels).map(
+                        ([key, value]) => (
+                          <SelectItem key={key} value={key}>
+                            {value}
+                          </SelectItem>
+                        )
+                      )}
                     </Select>
                   </div>
+                  {(currentUser?.role === Role.ORGANIZATION ||
+                    role === "user") && (
+                    <div>
+                      <Select
+                        variant="bordered"
+                        className="dark text-white"
+                        classNames={{
+                          trigger: "!border-white rounded-[1em]",
+                        }}
+                        label="Workshop"
+                        value={workshopId}
+                        onChange={(e) => setWorkshopId(e.target.value)}
+                      >
+                        {workshop?.map((workshop, index) => (
+                          <SelectItem key={index} value={workshop.id}>
+                            {workshop.fantasy_name}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                    </div>
+                  )}
                 </div>
               </ModalBody>
               <ModalFooter>
-                <Button color="default" variant="light" className="rounded-full px-5 text-white" onClick={onClose}>
+                <Button
+                  color="default"
+                  variant="light"
+                  className="rounded-full px-5 text-white"
+                  onClick={onClose}
+                >
                   Cancelar
                 </Button>
-                <Button color="success" className={styles.modalButton} onClick={addUser} disabled={loading}>
+                <Button
+                  color="success"
+                  className={styles.modalButton}
+                  onClick={addUser}
+                  disabled={loading}
+                >
                   Adicionar
                 </Button>
               </ModalFooter>
